c8f0680477fb6f3c11f0e7f107e134b070af3929
Omar3011 <om641773@gmail.com>
2025-03-14 02:58:43 +0200

    create custom app bar and custom search icon

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static final Color componentColor = Colors.black;
+}
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_appp/constants/app_constants.dart';
 import 'package:notes_appp/widgets/custom_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -9,6 +10,38 @@ class NotesViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: const Column(children: [SizedBox(height: 50), CustomAppBar()]),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter tips',
+              style: TextStyle(color: AppConstants.componentColor),
+            ),
+            subtitle: Text(
+              'Builde your career with omar mostafa',
+              style: TextStyle(color: AppConstants.componentColor),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, color: AppConstants.componentColor),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
